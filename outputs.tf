86d31e49f9f26536c5285b1803f539498cb3676d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "storage_syncs" {
-  description = "All storage_sync resources"
-  value       = azurerm_storage_sync.storage_syncs
-}
 output "storage_syncs_incoming_traffic_policy" {
-  description = "List of incoming_traffic_policy values across all storage_syncs"
-  value       = [for k, v in azurerm_storage_sync.storage_syncs : v.incoming_traffic_policy]
+  description = "Map of incoming_traffic_policy values across all storage_syncs, keyed the same as var.storage_syncs"
+  value       = { for k, v in azurerm_storage_sync.storage_syncs : k => v.incoming_traffic_policy }
 }
 output "storage_syncs_location" {
-  description = "List of location values across all storage_syncs"
-  value       = [for k, v in azurerm_storage_sync.storage_syncs : v.location]
+  description = "Map of location values across all storage_syncs, keyed the same as var.storage_syncs"
+  value       = { for k, v in azurerm_storage_sync.storage_syncs : k => v.location }
 }
 output "storage_syncs_name" {
-  description = "List of name values across all storage_syncs"
-  value       = [for k, v in azurerm_storage_sync.storage_syncs : v.name]
+  description = "Map of name values across all storage_syncs, keyed the same as var.storage_syncs"
+  value       = { for k, v in azurerm_storage_sync.storage_syncs : k => v.name }
 }
 output "storage_syncs_registered_servers" {
-  description = "List of registered_servers values across all storage_syncs"
-  value       = [for k, v in azurerm_storage_sync.storage_syncs : v.registered_servers]
+  description = "Map of registered_servers values across all storage_syncs, keyed the same as var.storage_syncs"
+  value       = { for k, v in azurerm_storage_sync.storage_syncs : k => v.registered_servers }
 }
 output "storage_syncs_resource_group_name" {
-  description = "List of resource_group_name values across all storage_syncs"
-  value       = [for k, v in azurerm_storage_sync.storage_syncs : v.resource_group_name]
+  description = "Map of resource_group_name values across all storage_syncs, keyed the same as var.storage_syncs"
+  value       = { for k, v in azurerm_storage_sync.storage_syncs : k => v.resource_group_name }
 }
 output "storage_syncs_tags" {
-  description = "List of tags values across all storage_syncs"
-  value       = [for k, v in azurerm_storage_sync.storage_syncs : v.tags]
+  description = "Map of tags values across all storage_syncs, keyed the same as var.storage_syncs"
+  value       = { for k, v in azurerm_storage_sync.storage_syncs : k => v.tags }
 }
 
